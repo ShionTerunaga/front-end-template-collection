a243d699389425b9358462f7d3407fcb006dff16
Shion Terunaga <shion.terunaga@ShionnoMacBook-Pro.local>
2025-12-13 11:24:34 +0900

    fix: copy template

--- execution/win/tmp/template/react/tanstack-router/vanilla-extract/src/utils/result.ts
+++ execution/win/tmp/template/react/tanstack-router/vanilla-extract/src/utils/result.ts
@@ -1,130 +1,130 @@
 const basic = {
-    RESULT_OK: "ok",
-    RESULT_NG: "ng"
-} as const;
+    RESULT_OK: 'ok',
+    RESULT_NG: 'ng',
+} as const
 
 interface OK<T> {
-    readonly kind: typeof basic.RESULT_OK;
-    readonly value: T;
+    readonly kind: typeof basic.RESULT_OK
+    readonly value: T
 }
 
 interface NG<E> {
-    readonly kind: typeof basic.RESULT_NG;
-    readonly err: E;
+    readonly kind: typeof basic.RESULT_NG
+    readonly err: E
 }
 
 interface CheckResultReturn<T, E> {
-    fn: () => NonNullable<T>;
-    err: NonNullable<E>;
+    fn: () => NonNullable<T>
+    err: NonNullable<E>
 }
 
 interface CheckResultVoid<E> {
-    fn: () => void;
-    err: NonNullable<E>;
+    fn: () => void
+    err: NonNullable<E>
 }
 
 interface CheckPromiseReturn<T, E> {
-    fn: () => Promise<NonNullable<T>>;
-    err: NonNullable<E>;
+    fn: () => Promise<NonNullable<T>>
+    err: NonNullable<E>
 }
 
 interface CheckPromiseVoid<E> {
-    fn: () => Promise<void>;
-    err: NonNullable<E>;
+    fn: () => Promise<void>
+    err: NonNullable<E>
 }
 
-const UNIT_SYMBOL = Symbol("UNIT_SYMBOL");
+const UNIT_SYMBOL = Symbol('UNIT_SYMBOL')
 
 interface Unit {
-    readonly _unit: typeof UNIT_SYMBOL;
+    readonly _unit: typeof UNIT_SYMBOL
 }
 
-export type Result<T, E> = OK<NonNullable<T>> | NG<NonNullable<E>>;
+export type Result<T, E> = OK<NonNullable<T>> | NG<NonNullable<E>>
 
 export const resultUtility = (function () {
-    const { RESULT_NG, RESULT_OK } = basic;
+    const { RESULT_NG, RESULT_OK } = basic
 
     const UNIT: Unit = Object.freeze({
-        _unit: UNIT_SYMBOL
-    });
+        _unit: UNIT_SYMBOL,
+    })
 
     const checkPromiseReturn = async <T, E>({
         fn,
-        err
+        err,
     }: CheckPromiseReturn<T, E>): Promise<Result<T, E>> => {
         try {
-            const result = await fn();
+            const result = await fn()
 
-            return createOk(result);
+            return createOk(result)
         } catch {
-            return createNg(err);
+            return createNg(err)
         }
-    };
+    }
 
     const checkPromiseVoid = async <E>({
         fn,
-        err
+        err,
     }: CheckPromiseVoid<E>): Promise<Result<Unit, E>> => {
         try {
-            await fn();
+            await fn()
 
-            return createOk(UNIT);
+            return createOk(UNIT)
         } catch {
-            return createNg(err);
+            return createNg(err)
         }
-    };
+    }
 
     const checkResultReturn = <T, E>({
         fn,
-        err
+        err,
     }: CheckResultReturn<T, E>): Result<T, E> => {
         try {
-            const result = fn();
+            const result = fn()
 
-            return createOk(result);
+            return createOk(result)
         } catch {
-            return createNg(err);
+            return createNg(err)
         }
-    };
+    }
 
     const checkResultVoid = <E>({
         fn,
-        err
+        err,
     }: CheckResultVoid<E>): Result<Unit, E> => {
         try {
-            fn();
+            fn()
 
-            return createOk(UNIT);
+            return createOk(UNIT)
         } catch {
-            return createNg(err);
+            return createNg(err)
         }
-    };
+    }
 
     const isOK = <T extends NonNullable<unknown>, E>(
-        res: Result<T, E>
+        res: Result<T, E>,
     ): res is OK<T> => {
-        return res.kind === RESULT_OK;
-    };
+        return res.kind === RESULT_OK
+    }
 
     const isNG = <T, E extends NonNullable<unknown>>(
-        res: Result<T, E>
+        res: Result<T, E>,
     ): res is NG<E> => {
-        return res.kind === RESULT_NG;
-    };
+        return res.kind === RESULT_NG
+    }
 
     const createOk = <T>(value: NonNullable<T>): Result<T, never> => {
         return Object.freeze({
             kind: RESULT_OK,
-            value
-        });
-    };
+            value,
+        })
+    }
 
     const createNg = <E>(err: NonNullable<E>): Result<never, E> => {
         return Object.freeze({
             kind: RESULT_NG,
-            err
-        });
-    };
+            err,
+        })
+    }
 
     return Object.freeze({
         UNIT,
@@ -135,6 +135,6 @@ export const resultUtility = (function () {
         isOK,
         isNG,
         createOk,
-        createNg
-    });
-})();
+        createNg,
+    })
+})()
