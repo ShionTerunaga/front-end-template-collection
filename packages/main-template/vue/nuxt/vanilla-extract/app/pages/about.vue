3f6ef5d1106e5bc11cab649261bb2181e33bedd1
ShionTerunaga <sion2769@gmail.com>
2026-01-01 02:15:22 +0900

    feat: move app to src/app

--- packages/main-template/vue/nuxt/vanilla-extract/app/pages/about.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <section>
-        <h1>About</h1>
-        <p>This page is statically rendered by Nuxt.</p>
-    </section>
-</template>
--- packages/main-template/vue/nuxt/vanilla-extract/nuxt.config.ts
+++ packages/main-template/vue/nuxt/vanilla-extract/nuxt.config.ts
@@ -2,6 +2,7 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    srcDir: "src/app/",
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
     vite: {
