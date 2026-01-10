d1fdad2dfbdf8c36cb29438886014218b7fb6582
ShionTerunaga <sion2769@gmail.com>
2026-01-10 15:19:00 +0900

    feat: set random dog services

--- packages/main-template/react/react-router/vanilla-extract/app/shared/config/config.ts
+++ packages/main-template/react/react-router/vanilla-extract/app/shared/config/config.ts
@@ -2,4 +2,5 @@ import { envParse } from '@/utils/env-parse'
 
 export const appConfig = {
     apiKey: envParse(import.meta.env.VITE_API_KEY),
+    apiKey2: envParse(import.meta.env.VITE_API_KEY2),
 }
