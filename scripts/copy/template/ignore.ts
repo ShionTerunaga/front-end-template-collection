5572c6d90589e0d6150ef9b0f99ee956c0d2284e
ShionTerunaga <sion2769@gmail.com>
2026-01-16 16:43:21 +0900

    fix: compiled the files to be omitted

---
+++ scripts/copy/template/ignore.ts
@@ -0,0 +1,12 @@
+export const ignores = [
+    "node_modules",
+    ".next",
+    "pnpm-lock.yaml",
+    "dist",
+    "coverage",
+    ".tanstack",
+    ".output",
+    ".nuxt",
+    ".react-router",
+    "build"
+];
